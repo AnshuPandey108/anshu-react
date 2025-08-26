14b02a4382c0fc4d23e5b755b77ea8d2d02dbd89
Anshu Pandey <anshu.pandey@avista.com>
2025-08-26 16:28:44 +0530

    Context Api  creation basic

---
+++ minicontext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useContext} from 'react';
+import UserContext from '../context/UserContext';
+
+const Profile = () => {
+      const {user} =useContext(UserContext)
+      console.log(user)
+    if(!user) return <div>Please Login</div>
+    return <div>Welcome {user.userName}</div>
+}
+
+export default Profile;
+
